061c276e4e182a3d6def0648490f83c8d2a934ce
sridharamar57 <80243218+sridharamar57@users.noreply.github.com>
2021-03-16 10:36:25 +0530

    Update OrderRepository.java

--- src/main/java/com/infy/order/repository/OrderRepository.java
+++ src/main/java/com/infy/order/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.infy.order.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.infy.order.entity.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Integer>{
 
+public interface OrderRepository extends JpaRepository<Order, Integer>{
+	List<Order> findByBuyerId(Integer buyerId);
 }
